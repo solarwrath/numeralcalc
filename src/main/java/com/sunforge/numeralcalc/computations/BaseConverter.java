a8a759f9f307e4dd3efd1f50f69894742d1c2a81
Антон Жлуктарёв <kilpatricktheraptor@gmail.com>
2018-11-02 15:38:09 +0200

    Implemented converting from base10 to custom base

--- src/main/java/com/sunforge/numeralcalc/computations/BaseConverter.java
+++ src/main/java/com/sunforge/numeralcalc/computations/BaseConverter.java
@@ -1,4 +1,39 @@
 package com.sunforge.numeralcalc.computations;
 
 public class BaseConverter {
+    public static int convertToBase10(int givenInteger, int givenBase) {
+        int result = 0;
+        int counter = 0;
+
+        //See converting to base10
+        while (givenInteger >= 1) {
+            result += (int) (Math.pow(givenBase, counter) * (givenInteger % 10));
+            givenInteger /= 10;
+            counter++;
+        }
+        return result;
+    }
+
+    public static int convertFromBase10(int givenInteger, int givenBase) {
+        int result = 0, currentDiv = 0;
+
+        //Divide and get mods
+        while (givenInteger >= givenBase) {
+            currentDiv = Math.floorDiv(givenInteger, givenBase);
+            result = result * 10 + (givenInteger - (currentDiv * givenBase));
+            givenInteger = currentDiv;
+        }
+        //Append the latest mod
+        result = result * 10 + currentDiv;
+        return reverseNumber(result);
+    }
+
+    private static int reverseNumber(int givenNumber) {
+        int reversed = 0;
+        while (givenNumber > 0) {
+            reversed = reversed * 10 + givenNumber % 10;
+            givenNumber = Math.floorDiv(givenNumber, 10);
+        }
+        return reversed;
+    }
 }
--- src/main/java/com/sunforge/numeralcalc/computations/MainHandler.java
+++ src/main/java/com/sunforge/numeralcalc/computations/MainHandler.java
@@ -1,14 +1,14 @@
 package com.sunforge.numeralcalc.computations;
 
 
+import static com.sunforge.numeralcalc.computations.BaseConverter.convertFromBase10;
+import static com.sunforge.numeralcalc.computations.BaseConverter.convertToBase10;
+
 public class MainHandler {
 
-    public static int compute(int number1, int number2, int base1, int base2, Operation requestedOperation) {
-        System.out.println(number1);
-        System.out.println(number2);
-        System.out.println(base1);
-        System.out.println(base2);
-        System.out.println(requestedOperation.toString());
+    public static int compute(int number1, int base1, int number2, int base2, Operation requestedOperation) {
+        int number1InBase10 = convertToBase10(number1, base1);
+        int number1Converted = convertFromBase10(number1InBase10, base2);
         return 0;
     }
 }
